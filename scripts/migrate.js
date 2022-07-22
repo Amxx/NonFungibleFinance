52d9787fa5752b85007d2a506862a56d8dd5cbcf
Hadrien Croubois <hadrien.croubois@gmail.com>
2022-07-22 21:32:33 +0200

    Initial commit

---
+++ scripts/migrate.js
@@ -0,0 +1,43 @@
+const { MigrationManager, getFactory, attach } = require('@amxx/hre/scripts');
+const DEBUG = require('debug')('VV');
+
+upgrades.silenceWarnings();
+
+async function migrate(env = {}) {
+
+    const provider = env.provider || ethers.provider;
+    const signer   = env.signer   || await ethers.getSigner();
+    const network  = await ethers.provider.getNetwork();
+    const manager  = new MigrationManager(provider);
+    signer.address = await signer.getAddress();
+
+    DEBUG(`network:     ${network.name} (${network.chainId})`);
+    DEBUG(`signer:      ${signer.address}`);
+
+    // Put known addresses into the cache
+    await manager.ready().then(() => Promise.all(Object.entries(env[network.chainId] || {}).map(([ name, address ]) => manager.cache.set(name, address))));
+    const opts = { noCache: config.noCache || network.chainId == 31337, noConfirm: config.noConfirm };
+
+    /*******************************************************************************************************************
+     *                                                      Token                                                      *
+     *******************************************************************************************************************/
+    const factory = await manager.migrate(
+        'VestingFactory',
+        getFactory('VestingFactory', { signer }),
+        { ...opts },
+    );
+    DEBUG(`VestingFactory: ${factory.address }`);
+}
+
+if (require.main === module) {
+    const ENV = require('./env');
+
+    migrate(ENV)
+        .then(() => process.exit(0))
+        .catch(error => {
+            console.error(error);
+            process.exit(1);
+        });
+}
+
+module.exports = migrate;
